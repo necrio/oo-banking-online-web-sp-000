16fe9df210b3d31a1cf8960be701268ece5acbfe
Keith Isham <kisham8899@gmail.com>
2019-05-18 13:36:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,6 +33,14 @@ else
 end
 end
 
+def reverse_transfer
+  if @status = "complete"
+    @sender.deposit( @amount )
+    @receiver.deposit( @amount * -1)
+    @status = "reversed"
+  end
+end
+
 
 
 
