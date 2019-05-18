9720f18e2c083d834c8f8bf54dd7c7505fc15f33
Keith Isham <kisham8899@gmail.com>
2019-05-18 13:40:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,45 +20,23 @@ def valid?
   end
 end
 
-# def execute_transaction
-#   if @sender.balance < @amount
-#     @status = "rejected"
-#   return "Transaction rejected. Please check your account balance."
-# elsif  @status == "Complete"
-#   puts "complete!"
-# else
-#   @sender.deposit( @amount * -1)
-#   @receiver.deposit (@amount)
-#   @status = "complete"
-# end
-# end
-#
-# def reverse_transfer
-#   if @status = "complete"
-#     @sender.deposit(@amount)
-#     @receiver.deposit(@amount*-1)
-#     @status = "reversed"
-#   end
-# end
-
 def execute_transaction
   if @sender.balance < @amount
     @status = "rejected"
-    return "Transaction rejected. Please check your account balance."
-
-  elsif @status == "complete"
-    puts "Transaction was already excuted"
-  else
-    @sender.deposit( @amount * -1 )
-    @receiver.deposit( @amount )
-    @status = "complete"
-  end
+  return "Transaction rejected. Please check your account balance."
+elsif  @status == "Complete"
+  puts "complete!"
+else
+  @sender.deposit( @amount * -1)
+  @receiver.deposit ( @amount )
+  @status = "complete"
+end
 end
 
 def reverse_transfer
-  if @status == "complete"
+  if @status = "complete"
     @sender.deposit( @amount )
-    @receiver.deposit( @amount * -1)
+    @receiver.deposit( @amount * -1 )
     @status = "reversed"
   end
 end
