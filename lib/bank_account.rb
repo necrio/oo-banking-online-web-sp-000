0be190c3be84e3c3f29fe417f2e95bbda5fdf2ce
Keith Isham <kisham8899@gmail.com>
2019-05-18 13:10:16 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name)
+    @balance = 1000
+    @name = name
+    @status = "open"
+  end
+
+
+
+end
+
+
+
+
+
+
+class Transfer
+  # your code here
 end
